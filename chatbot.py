d94c636cc07ee71ecc79f2760b1aae6495e06510
adrienckr <127618020+adrienckr@users.noreply.github.com>
2023-05-16 20:47:36 +0100

    Update chatbot.py
    Helper function designed for chatbot.

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
 import openai 
-openai.api_key='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'
+openai.api_key='YOUR API KEY '
 
 
 #Throughout this tuto, we will use OpenAI's gpt-3.5-turbo model
@@ -18,22 +18,43 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-text = f"""
-You should express what you want a model to do by \ 
-providing instructions that are as clear and \ 
-specific as you can possibly make them. \ 
-This will guide the model towards the desired output, \ 
-and reduce the chances of receiving irrelevant \ 
-or incorrect responses. Don't confuse writing a \ 
-clear prompt with writing a short prompt. \ 
-In many cases, longer prompts provide more clarity \ 
-and context for the model, which can lead to \ 
-more detailed and relevant outputs.
-"""
-prompt = f"""
-Summarize the text delimited by triple backticks \ 
-into a single sentence.
-```{text}```
-"""
-response = get_completion(prompt)
+# text = f"""
+# You should express what you want a model to do by \ 
+# # providing instructions that are as clear and \ 
+# specific as you can possibly make them. \ 
+# This will guide the model towards the desired output, \ 
+# and reduce the chances of receiving irrelevant \ 
+# or incorrect responses. Don't confuse writing a \ 
+# clear prompt with writing a short prompt. \ 
+# In many cases, longer prompts provide more clarity \ 
+# and context for the model, which can lead to \ 
+# more detailed and relevant outputs.
+# """
+# prompt = f"""
+# Summarize the text delimited by triple backticks \ 
+# into a single sentence.
+# ```{text}```
+# """
+# response = get_completion(prompt)
+# print(response)
+
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
+
+
+
+messages =  [  
+{'role':'system', 'content':'You are an assistant that speaks like Shakespeare.'},    
+{'role':'user', 'content':'tell me a joke'},   
+{'role':'assistant', 'content':'Why did the chicken cross the road'},   
+{'role':'user', 'content':'I don\'t know'}  ]
+
+response = get_completion_from_messages(messages, temperature=1)
 print(response)
